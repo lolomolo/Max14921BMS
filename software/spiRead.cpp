86ef19fe282c65db1693b6abdfa51b3a69429fbb
Sam Ragsdale <sam.ragsdale@wustl.edu>
2018-04-15 23:50:26 +0000

    spi interface to Raspberry Pi

---
+++ software/spiRead.cpp
@@ -0,0 +1,104 @@
+/******************************************************************************
+i2ctest.cpp
+Raspberry Pi I2C interface demo
+Byron Jacquot @ SparkFun Electronics>
+4/2/2014
+https://github.com/sparkfun/Pi_Wedge
+
+A brief demonstration of the Raspberry Pi I2C interface, using the SparkFun
+Pi Wedge breakout board.
+
+Resources:
+
+This example makes use of the Wiring Pi library, which streamlines the interface
+to the the I/O pins on the Raspberry Pi, providing an API that is similar to the
+Arduino.  You can learn about installing Wiring Pi here:
+http://wiringpi.com/download-and-install/
+
+The wiringPi SPI API is documented here:
+https://projects.drogon.net/raspberry-pi/wiringpi/spi-library/
+
+The init call returns a standard file descriptor.  More detailed configuration
+of the interface can be performed using ioctl calls on that descriptor.
+See the wiringPi SPI implementation (wiringPi/wiringPiSPI.c) for some examples.
+Parameters configurable with ioctl are documented here:
+http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/tree/Documentation/spi/spidev
+
+Hardware connections:
+
+This file interfaces with the SparkFun Serial 7 Segment display:
+https://www.sparkfun.com/products/11629
+
+The board was connected as follows:
+(Raspberry Pi)(Serial 7 Segment)
+GND  -> GND
+3.3V -> Vcc
+CE1  -> SS (Shift Select)
+SCK  -> SCK 
+MOSI -> SDI
+MISO -> SDO
+
+To build this file, I use the command:
+>  g++ spitest.cpp -lwiringPi
+
+Then to run it, first the spi kernel module needs to be loaded.  This can be 
+done using the GPIO utility.
+> gpio load spi
+> ./a.out
+
+This test uses the single-segment mode of the 7 segment display.  It shifts a 
+bit through the display characters, lighting a single character of each at a 
+time.
+
+Development environment specifics:
+Tested on Raspberry Pi V2 hardware, running Raspbian.
+Building with GCC 4.6.3 (Debian 4.6.3-14+rpi1)
+
+This code is beerware; if you see me (or any other SparkFun employee) at the
+local, and you've found our code helpful, please buy us a round!
+
+Distributed as-is; no warranty is given.
+******************************************************************************/
+
+#include <iostream>
+#include <errno.h>
+#include <wiringPiSPI.h>
+#include <unistd.h>
+#include <bitset>
+using namespace std;
+
+// channel is the wiringPi name for the chip select (or chip enable) pin.
+// Set this to 0 or 1, depending on how it's connected.
+static const int CHANNEL = 1;
+
+int main()
+{
+   int fd, result;
+   unsigned char buffer[100];
+
+   cout << "Initializing" << endl;
+
+   // Configure the interface.
+   // CHANNEL insicates chip select,
+   // 500000 indicates bus speed.
+   fd = wiringPiSPISetup(CHANNEL, 500000);
+
+   cout << "Init result: " << fd << endl;
+
+   // clear display
+   buffer[0] = 0b01010101;
+   buffer[1] = 0b00001010;
+
+   for(int i = 0; i<10; i++) {
+	buffer[0]=0xff; 
+  	cout << "buffer[0]: " << std::bitset<8>(buffer[0]) << endl;
+   	cout << "buffer[1]: " << std::bitset<8>(buffer[1]) << endl;
+   	wiringPiSPIDataRW(CHANNEL, buffer, 2);
+   	cout << "new buffer[0]: " << std::bitset<8>(buffer[0]) << endl;
+   	cout << "new buffer[1]: " << std::bitset<8>(buffer[1]) << endl;
+	long aout = buffer[1]+(buffer[0]<<8);
+	cout << "voltage reading: " << aout << endl << endl;
+   	usleep(100000);
+   }
+
+}
